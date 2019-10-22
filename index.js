cfc23afb6909472930924b2f4edd88235264a553
hyunalee <dlgusdk419@naver.com>
2019-10-22 16:59:56 +0900

    docs: add comment

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+// AppRegistry 에 root Component 를 등록하여 ReactNative 에게 알려준다.
 AppRegistry.registerComponent(appName, () => App);
